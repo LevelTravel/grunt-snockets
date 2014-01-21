521f8e5b4b24082dba2f1c82d281303e2267f490
Nicholas <nr@fenelon.ru>
2014-01-21 12:47:44 +0400

    fixing guid()

--- tasks/snockets.js
+++ tasks/snockets.js
@@ -20,7 +20,7 @@ module.exports = function(grunt) {
   // ==========================================================================
 
   var guid = function(){
-    var s4 = function(){ Math.floor((1 + Math.random()) * 0x10000).toString(16).substring(1); };
+    var s4 = function(){ return Math.floor((1 + Math.random()) * 0x10000).toString(16).substring(1); };
     return s4() + s4() + '-' + s4() + '-' + s4() + '-' + s4() + '-' + s4() + s4() + s4();
   }
 
